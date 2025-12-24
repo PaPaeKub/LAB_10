6e1a83e820642dc2bbee9cb87f66f6b2bd55682a
Peerawish <paepeerawishmakheaw@gmail.com>
2025-12-25 00:30:00 +0700

    25/12/2568

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,44 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream file("score.txt");
+    double value;
+    double sum = 0.0;
+    double mean = 0.0;
+    double sd = 0.0;
+    int count = 0;
+
+    while (file >> value) {
+        sum += value;
+        count++;
+    }
+
+    mean = sum / count;
+
+    file.clear();
+    file.seekg(0, ios::beg);
+
+    double varianceSum = 0.0;
+    while (file >> value) {
+        varianceSum += pow(value - mean, 2);
+    }
+
+    sd = sqrt(varianceSum / count);
+
+    file.close();
+
+    cout << "Number of data = " << count << endl;
+
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+
+    cout << fixed << setprecision(3);
+    cout << "Standard deviation = " << sd << endl;
+
+    return 0;
+}
